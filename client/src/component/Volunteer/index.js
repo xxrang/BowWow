240f38e0fbbdd1c484f580ff2cba472d23774287
이숙영 <22sook00@gmail.com>
2021-08-26 00:15:09 +0900

    add volunteer
    add volunteer structure

--- client/src/component/Volunteer/index.js
+++ client/src/component/Volunteer/index.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React,{useState} from 'react'
+import VolunteerList from './VolunteerList'
+import dummySample from '../Service/dummySample'
 import {StyledVolunteer} from './StyledVoluteer'
 
+
 const Volunteer = () => {
+
+  const [volunteerDog, setVolunteerDog] = useState(dummySample)
   return (
     <StyledVolunteer>
-      volunteer
+      <VolunteerList volunteerDog = {volunteerDog}/>
+      <button>More</button>
     </StyledVolunteer>
   )
 }
