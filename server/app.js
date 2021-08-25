ed8018edf2d919e8d670186ce448e25b779c4d17
xxrrang <xxrrang2@gmail.com>
2021-08-26 01:30:01 +0900

    app파일 수정

--- server/app.js
+++ server/app.js
@@ -1,10 +1,6 @@
 const express = require("express");
 const app = express();
-<<<<<<< HEAD
 const port = 80;
-=======
-const port = 4000;
->>>>>>> 1f03379c512c2fd39d7e1ba71102f535491a4f8c
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
