67ea55a737f81145b163a45f3b8c7c71bba17345
xxrrang <xxrrang2@gmail.com>
2021-08-27 02:04:02 +0900

    <feat>: <user login API>
    user login API 작성
    
    user login API 작성

--- server/controllers/users/Login.js
+++ server/controllers/users/Login.js
@@ -1,3 +1,16 @@
+const { user } = require('../../models');
+
 module.exports = async (req, res) => {
     
+ const { email, password } = req.body;
+
+ user.findOne({ where: {email, password}})
+ .then((data) => {
+     if(!data){
+         res.status(404).send({message: 'invalid user'})
+     } else {
+         res.status(200).send({message: 'login ok'})
+     }
+ })
+ 
 }
