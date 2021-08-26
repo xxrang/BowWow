d3c33f4282d5b18b4e60129a6c6ae1d7793f97e9
xxrrang <xxrrang2@gmail.com>
2021-08-27 02:05:11 +0900

    <feat>: <user signup API>
    user signup API 작성
    
    user signup API 작성

--- server/controllers/users/SignUp.js
+++ server/controllers/users/SignUp.js
@@ -1,3 +1,31 @@
+const { user } = require('../../models');
+
 module.exports = async (req, res) => {
-    
+
+ const { email, nickname, password, introduce, image, createdAt } = req.body;
+
+ if(!email || !password ){
+     return res.status(422).json({message: 'insufficient parameters supplied'});
+     //클라이언트에서 이메일하고 비번 안 넘어온다면
+ }
+
+ const userCheck = await user.findOne({ where: { email }})
+    .then((duplicate_User) => {
+        if(duplicate_User){
+            res.status(409).send({message: 'email exists'}) //계정 중복 가입
+        } else {
+            const userCreate = user.create({
+                email: email,
+                nickname: nickname,
+                password: password,
+                introduce: introduce,
+                image: image,
+                createdAt: createdAt
+            }).then((user) => {
+               if(user){
+                   res.status(201).send({message: 'ok'})
+               }
+            })
+        }
+    })
 }
