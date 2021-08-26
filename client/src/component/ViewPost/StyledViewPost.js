8f6c549207ca4df81a198e52ed431e82690b6502
이숙영 <22sook00@gmail.com>
2021-08-26 17:03:22 +0900

    edit viewpost
    edit viewpost

--- client/src/component/ViewPost/StyledViewPost.js
+++ client/src/component/ViewPost/StyledViewPost.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 export const StyledViewPost = styled.div`
   margin : 50px 220px;
   display: grid;
-  grid-template-columns: 2fr 1fr;
+  grid-template-columns: 2fr 1fr  ;
   grid-template-rows: 250px 250px; 
 `
 export const StyledViewPostContent = styled.div`
