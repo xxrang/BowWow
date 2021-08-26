c449e1f459c9dc0c0515a9296fac49f69c6e22ac
dydwns2441 <heoyjun6@gmail.com>
2021-08-26 21:34:13 +0900

    feat/viewpost feat/viewpost 컴포넌트 content부분 완료

--- client/src/component/ViewPost/ViewPostContent.js
+++ client/src/component/ViewPost/ViewPostContent.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import {StyledViewPostContent} from './StyledViewPost'
+import ViewPostProfile from './ViewPostProfile'
+import ViewPostContentOne from "./ViewPostContentOne";
 
 function ViewPostContent() {
   return (
     <StyledViewPostContent>
-      ViewPostContent
+      <ViewPostProfile />
+      <ViewPostContentOne />
     </StyledViewPostContent>
   )
 }
