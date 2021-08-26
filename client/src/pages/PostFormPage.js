d424c8ba94b2dc897ca75d95d2733110ae4ffca8
dydwns2441 <heoyjun6@gmail.com>
2021-08-26 16:10:54 +0900

    feat/postform feat/postform 포스트폼 컴포넌트 틀 작성완료, 3개의 컴포넌트로 나눔.

--- client/src/pages/PostFormPage.js
+++ client/src/pages/PostFormPage.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import NavBar from '../component/NavBar';
 import Footer from '../component/Footer';
+import PostForm from '../component/PostForm';
 
 const PostFormPage = () => {
   return <>
     <NavBar />
+    <PostForm />
     <Footer />
   </>
 }
