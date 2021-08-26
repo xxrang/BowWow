69471104ce25ad32d901bc7610213c40737b8547
이숙영 <22sook00@gmail.com>
2021-08-26 21:34:14 +0900

    add : 포스트 코멘트
    포스트 코멘트 틀 작성

--- client/src/component/ViewPost/StyledViewPost.js
+++ client/src/component/ViewPost/StyledViewPost.js
@@ -3,14 +3,79 @@ import styled from 'styled-components'
 export const StyledViewPost = styled.div`
   margin : 50px 220px;
   display: grid;
-  grid-template-columns: 2fr 1fr  ;
-  grid-template-rows: 250px 250px; 
+  grid-template-columns: 2fr 1fr ;
+  /*grid-template-rows: 500px; */
+  height: 63vh;
 `
 export const StyledViewPostContent = styled.div`
-
 margin-right: 20px;
 `
 
 export const StyledViewPostComment = styled.div`
+/*width : 300px;
+height: 500px;*/
+/*background: rgb(220, 248, 235);*/
+display: flex;
+flex-direction: column;
+align-items: center;
+box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+padding : 20px;
+>.post-comment-form-wrapper{
+  width : 100%;
+  >i{
+    margin-left: 10px;
+  }
+  >.flex-box{
+    display: flex;
+    >.post-comment-input-submit{
+      width : 50px;
+      height : 32px;
+      background: #48cfad;
+      color : white;
+    }
+  }
+  >.flex-box>form >.post-comment-input,.post-comment-input-submit{
+    margin : 10px 5px;
+    height : 30px;
+    border-radius: 5px;
+    border : none;
+  }
+  .flex-box>form>.post-comment-text{
+      width : 200px;
+      padding-left: 10px;
+      border : 1px solid grey;
+  }
 
+  >.post-comment-wrapper{
+  width : 100%;
+  padding : 0 20px;
+}
+>.post-comment-wrapper>.post-comment-profile-wrapper{
+  display: flex;
+  align-items: center;
+  width : 88%;
+  padding-bottom: 10px;
+  border-bottom: 1px solid grey;
+  >h4{
+      margin-bottom: 12px;
+    }
+  >.profile-img{
+    width : 50px;height: 50px;
+    border-radius: 50%;
+    margin : 0 10px;
+  }
+}
+>.post-comment-wrapper{
+    margin: 10px;
+    padding : 0;
+    >ul{
+      padding : 0;
+      list-style: none;
+      >li{
+        margin-top : 10px;
+        padding : 10px;
+      }
+    }
+  }
+}
 `
--- client/src/component/ViewPost/index.js
+++ client/src/component/ViewPost/index.js
@@ -1,13 +1,43 @@
-import React from 'react'
+import React, { useState,useRef,useEffect } from 'react'
 import {StyledViewPost} from './StyledViewPost'
 import ViewPostContent from './ViewPostContent'
 import ViewPostComment from './ViewPostComment'
+import dummySample from './dummySample'
 
 function ViewPost() {
+
+  const [comment,setComment] = useState('')
+  const [commentList,setCommentList] = useState(dummySample)
+
+  const handleChangeComment =(e)=>{
+    //console.log(e.target.value);
+    setComment(e.target.value);
+  }
+  const submitComment = (e) => {
+    inputRef.current.focus();
+    e.preventDefault();
+    setCommentList([comment,...commentList])
+    setComment('')
+    console.log(comment);
+    console.log(commentList)
+  }
+  
+  //useRef 등록
+  const inputRef = useRef();
+  //useEffect
+  useEffect(()=>{
+    inputRef.current.focus();
+  })
+  
   return (
     <StyledViewPost>
       <ViewPostContent/>
-      <ViewPostComment/>
+      <ViewPostComment 
+      inputRef = {inputRef}
+      submitComment = {submitComment} 
+      comment = {comment} 
+      commentList = {commentList}
+      handleChangeComment = {handleChangeComment}/>
     </StyledViewPost>
   )
 }
