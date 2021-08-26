0ae7d2cd51c7c77cf87362c7c86ebbd02bb873f4
xxrrang <xxrrang2@gmail.com>
2021-08-26 11:17:43 +0900

    <feat>: <API 기본 구조 작성>
    API 기본 컨트롤러 작성
    
    API 요청 분기별 컨트롤러 작성 및 필요 라이브러리 설치

--- server/app.js
+++ server/app.js
@@ -1,10 +1,64 @@
 const express = require("express");
+const cors = require('cors')
+const cookieParser = require('cookie-parser');
+//express의 req객체에 cookies 속성 부여
 const app = express();
 const port = 80;
+require('dotenv').config();
+//env 환경변수 읽어냄
+
+const controllers = require('./controllers');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false })); 
+//req에 body 속성 추가해서 req.body 접근 가능하게 하고 중첩된 객체 표현 허용 x 객체 안에 객체 파싱할 수 있게 하려면 true로 변경
+
+app.use(cors({
+  origin: true,
+  credentials: true,
+  methods: ['GET','POST','OPTIONS','DELETE','PATCH']
+}))
+
+app.use(cookieParser());
+
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+
+/** 유저 관련 API **/
+app.post('/users/signup', controllers.signup); //회원가입
+app.post('/users/login', controllers.login); //로그인
+app.post('/users/logout', controllers.signout); //로그아웃
+app.get('/email_check', controllers.email_check); //이메일 중복확인
+app.get('/nickname', controllers.nickname); //닉네임 중복확인
+
+/** 유저 마이페이지 API **/
+app.get('/profile', controllers.profile); //마이페이지 확인
+app.patch('/profile', controllers.profile); //마이페이지 수정
+
+/** 인증 API **/
+app.get('/auth', controllers.auth); //토큰 인증 관련
+
+/** 지도 API **/
+app.get('/shelter', controllers.search); //지도 API
+
+/** 유기견 관련 API */
+app.get('/posts/service_list', controllers.service_list); //유기견 게시글 전체 혹은 하나만 보여주기
+app.post('/posts/service_list', controllers.service_list); //유기견 게시글 작성하기
+
+/** 봉사일정 관련 API */
+app.get('/posts/volunteer_list', controllers.volunteer_list); //봉사일정 게시글 전체 혹은 하나만 보여주기
+app.post('/posts/volunteer_list', controllers.volunteer_list); //봉사일정 게시글 작성하기
+app.delete('/posts', controllers.post_delete); //게시글 삭제
+
+/** 댓글 API **/
+app.post('/comments', controllers.comments); //댓글 작성
+app.delete('/comments', controllers.comments); //댓글 삭제
+app.patch('/comments', controllers.comments); //댓글 수정
+
+
 
 app.listen(port, () => {
   console.log(`app listening at http://localhost:${port}`);
--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,5 +2,13 @@ module.exports = {
     auth: require('./users/Auth'),
     signup: require('./users/SignUp'),
     login: require('./users/Login'),
-    signout: require('./users/SignOut')
+    signout: require('./users/SignOut'),
+    email_check: require('./check/email_check'),
+    profile: require('./users/Profile'),
+    nickname: require('./check/nickname'),
+    service_list: require('./posts/Service_list'),
+    volunteer_list: require('./posts/Volunteer_list'),
+    post_delete: require('./posts/Post_delete'),
+    comments: require('./comment/comments'),
+    search: require('./posts/Search')
 }
