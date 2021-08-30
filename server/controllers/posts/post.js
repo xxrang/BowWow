8bbc4de0c50dca189d400154808303b1129dd5ae
xxrrang <xxrrang2@gmail.com>
2021-08-30 15:16:13 +0900

    <fix>: <게시글 폼 수정>
    게시글 폼 수정

--- server/controllers/posts/post.js
+++ server/controllers/posts/post.js
@@ -1,14 +1,17 @@
-const { post } = require('../../models');
+const { post, user, comment} = require('../../models');
 
 module.exports = async (req, res) => {
         
-            await post.findOne({ where : { id: req.query.id }})
-            //req.query(URI?req.query)로 받아올건지 req.params(URI/:id)로 받아올건지 axios 요청시 의견조율해야함
-            .then((data) => {
-                if(data){
-                    res.status(200).send({message: 'load success', service : data})
+            const data1 = await post.findOne({ where : { id: req.query.id },
+             include: [{
+                 model: user,
+             }]}).then( async () => {
+                await comment.findOne({ where : { posts_id: req.query.id}})
+            }).then((data2) => {
+                if(data2){
+                    res.status(200).send({message: 'load success', service : {data1, data2}})
                 } else {
                     res.status(404).send({message: 'load fail'})
-                } 
+                } //아이디, 닉네임, 유저 이미지
             })
         }
