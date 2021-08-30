c724c43d8c8eb210621b2900db70b6f18034d733
xxrrang <xxrrang2@gmail.com>
2021-08-31 00:10:47 +0900

    <fix>: <model column 수정>

--- server/models/category.js
+++ server/models/category.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      models.category.hasMany(models.category_content,{foreignKey: "categoy_id", sourceKey: 'id'})
+      models.category.hasMany(models.category_content,{foreignKey: "category_id", sourceKey: 'id'})
     }
   };
   category.init({
