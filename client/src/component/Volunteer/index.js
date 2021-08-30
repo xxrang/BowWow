167cd5646543dc94fd50f74f1466475bb796c74e
이숙영 <22sook00@gmail.com>
2021-08-30 09:07:02 +0900

    fix : rechange
    fix : rechante

--- client/src/component/Volunteer/index.js
+++ client/src/component/Volunteer/index.js
@@ -8,7 +8,7 @@ const Volunteer = ({ postsData, navString, setPostId, postId }) => {
   const slice = postsData.slice(0, maxDogList);
   const moreButtonHandler = () => {
     setMaxDogList((prevValue) => prevValue + 6);
-    //console.log(slice)
+    
     console.log(`강아지가 ${maxDogList}마리씩 늘어나고있는중.`);
   };
 
