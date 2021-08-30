1035e445950d8583c0bf77027889041f1a06f0ab
xxrrang <xxrrang2@gmail.com>
2021-08-30 15:56:41 +0900

    <fix>: <subject>

--- server/controllers/posts/post.js
+++ server/controllers/posts/post.js
@@ -4,15 +4,23 @@ const nickname = require('../check/nickname');
 module.exports = async (req, res) => {
 
             const { id } = req.query
-            await post.findOne({ where : { id: id },
-             include: [{
-                 model: user,
-             }, {model: comment, attribute: nickname, email, image}]
-            }).then((data) => {
-                if(data){
-                    res.status(200).send({message: 'load success', service : {data}})
-                } else {
-                    res.status(404).send({message: 'load fail'})
-                } //아이디, 닉네임, 유저 이미지
-            })
+            // await post.findOne({ where : { id: id },
+            //  include: [{
+            //      model: user,
+            //  }, {model: comment, include: [{ model: user}, 
+            //     where : {}] }]
+            // }).then((data) => {
+            //     if(data){
+            //         res.status(200).send({message: 'load success', service : {data}})
+            //     } else {
+            //         res.status(404).send({message: 'load fail'})
+            //     } //아이디, 닉네임, 유저 이미지
+            // })
+            const data1 = await post.findOne({ where: { id: id } ,
+                include : [{ model: user }, {model: comment}]})
+            const data2 = await comment.findOne({ where: { posts_id: id },
+                include: [{ model: user }]})
+            
+                res.status(200).send({message: 'ok', data: {posts: data1, data2}})
+            
         }
