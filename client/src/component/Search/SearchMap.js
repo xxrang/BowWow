8b2a798b990e39bf9553b0c5ca5e31293512fa99
Oh-Myeongjae <dhaudwo1@daum.net>
2021-09-01 01:18:58 +0900

    맵테스트중

--- client/src/component/Search/SearchMap.js
+++ client/src/component/Search/SearchMap.js
@@ -1,24 +1,29 @@
 import React, { useRef, useEffect } from "react";
 import {StyledSearchMap} from './StyledSearch'
+const {kakao} = window;
 
-const options = {
-  //지도를 생성할 때 필요한 기본 옵션
-  center: new window.kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
-  level: 3, //지도의 레벨(확대, 축소 정도)
+var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+var options = { //지도를 생성할 때 필요한 기본 옵션
+	center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+	level: 3 //지도의 레벨(확대, 축소 정도)
 };
 
+var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
 
 function SearchMap() {
 
-  const container = useRef(null); //지도를 담을 영역의 DOM 레퍼런스
-
   useEffect(() => {
-    new window.kakao.maps.Map(container.current, options); //지도 생성 및 객체 리턴
-    return () => {};
-  }, []);
+    kakao.maps.load(() => {
+        let el = document.getElementById('map');
+        let map = new kakao.maps.Map(el, {
+            center: new kakao.maps.LatLng(33.450701, 126.570667)
+        })
+    })
 
+}, []);
+  
   return (
-    <StyledSearchMap className="map" ref={container}>
+    <StyledSearchMap id="map" ref={container}>
     </StyledSearchMap>
 
   )
